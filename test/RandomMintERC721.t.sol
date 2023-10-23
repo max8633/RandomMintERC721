2609e4c8679974473c02c9db99d66f9b5bf10f2a
MaxKuo <max2571112@gmail.com>
2023-10-23 12:57:18 +0800

    new update

--- test/RandomMintERC721.t.sol
+++ test/RandomMintERC721.t.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {RandomMintERC721} from "../src/RandomMintERC721.sol";
+import {MintERC721} from "../src/RandomMintERC721.sol";
 
 contract CounterTest is Test {
 
-    RandomMintERC721 public RandomMintERC721Contract;
+    MintERC721 public RandomMintERC721Contract;
     address user1;
     address user2;
 
     function setUp() public {
-        RandomMintNFTContract = new RandomMintNFT();
+        RandomMintERC721Contract = new MintERC721();
         user1 = makeAddr("user1");
         user2 = makeAddr("user2");
     }
@@ -19,16 +19,20 @@ contract CounterTest is Test {
     function test_isBlind_tokenURI() public {
         vm.startPrank(user1);
         RandomMintERC721Contract.mint(user1);
-        string memory isBlindTokenURI = RandomMintERC721Contract.tokenURI();
-        assertEq(isBlindTokenURI, "ipfs://QmUtEJYJbZu7wmT34fKBPTSzYLfMxwjzvaRjJ5dLQRjBVR");
+        // string memory isBlindTokenURI = RandomMintERC721Contract.tokenURI(1);
+        assertEq(RandomMintERC721Contract.tokenURI(1), "ipfs://QmUtEJYJbZu7wmT34fKBPTSzYLfMxwjzvaRjJ5dLQRjBVR");
         vm.stopPrank();
     }
 
     function test_reveal_tokenURI() public {
         vm.startPrank(user2);
         RandomMintERC721Contract.setIsBlind(false);
-        string memory revealTokenURI = RandomMintERC721Contract.tokenURI();
-        assertEq(isBlindTokenURI, "ipfs://QmWw9UEQ4gBGjsjKBE1g1UhbupkJicK8QRxTvUzbTrp26F");
+        vm.stopPrank;
+
+        vm.startPrank(user1);
+        RandomMintERC721Contract.mint(user1);
+        // string memory revealTokenURI = RandomMintERC721Contract.tokenURI(1);
+        assertEq(RandomMintERC721Contract.tokenURI(1), "ipfs://QmWw9UEQ4gBGjsjKBE1g1UhbupkJicK8QRxTvUzbTrp26F");
         vm.stopPrank();        
     }
 }
